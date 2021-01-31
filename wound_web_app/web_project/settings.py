e052d0224c214b557427416b16bf541100ab39f4
Kartik Rawool <55804665+kartikrawool@users.noreply.github.com>
2021-01-31 16:04:08 +0530

    Update settings.py

--- wound_web_app/web_project/settings.py
+++ wound_web_app/web_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u^^e)yr*-5^x^m-nj^n=l^59d)k-e0i)$+$1z@@pvd*!x+w#ta'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
